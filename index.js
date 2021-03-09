bba7350a1a5e3e3504cf55ca50972430fdfd1729
Fyuture <76669361+Fyuture@users.noreply.github.com>
2021-03-09 17:39:20 -0500

    💻 Leaked by oguhacker clan
    💻 Leaked by oguhacker clan

---
+++ index.js
@@ -0,0 +1,72 @@
+/* 
+    FLASHFLIP SOURCE CODE LEAKED BY PRO EITE HACKER @OGUHACKER8384384
+
+    GG KID / HACKERGANG69 ON TOP
+    HAD TO KICK MY OWN BLOOD OUT, THEY MUSTEV FORGOT I WAS A CRIP GANG GANG SUWOO SWOOP SWOOP
+*/
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+console.clear()
+bloodout()
+
+function bloodout() {
+    crip()
+}
+function crip() {
+console.log(`
+
+*== Welcome To FlashFlip (LEAKED BY OGUGOD8384) ==*
+`)
+console.log("                                                 ")
+
+// this is where the magic happens for ppl who care
+rl.question(`FF | Heads Or Tails: `, leakedbyoguskid123 => {
+    try {
+        const funTip = ["Fun Factt: Did you know flashflip uses Math.random()? Provably Fair? More like probably fair", "Fun Fact: Did you know the owner of FlashFlip has used the money he scammed off of FlashFlip players on Happy Meals at his local McDonalds?", ""]
+        const oguhackerlol = ['Heads', 'Tails']
+        const getskidaddled = oguhackerlol[Math.round(Math.random())]
+        const randomFunTip = funTip[Math.round(Math.random())]
+        if (leakedbyoguskid123 == "bal") {
+
+        }
+        else if (getskidaddled == "Heads" & leakedbyoguskid123 == "heads") {
+            console.log(`
+
+            Congratulations, You have won.
+            `)
+        } else if (getskidaddled == "Heads" & leakedbyoguskid123 == "tails") {
+            console.log(`
+            
+            Congratulations, YOU LOST UR SHIT U GAY DUMB NIG-
+            `)
+            console.log(randomFunTip)
+        } else if (getskidaddled == "Tails" & leakedbyoguskid123 == "tails") {
+            console.log(`
+            
+            Congratulations, You have won.
+            `)
+            console.log(randomFunTip)
+        } else if (getskidaddled == "Tails" & leakedbyoguskid123 == "heads") {
+            console.log(`
+            
+            Congratulations, YOU LOST UR SHIT U GAY DUMB NIG-
+            `)
+            console.log(randomFunTip)
+        } else {
+            console.log(`
+            
+            An error has occured. Please ensure you're using the program properly. Ex: [heads], [tails], [bal]
+            `)
+        }
+        // i just do this to have it restart
+    } finally {
+        bloodout()
+    }
+})
+}
+
+// .gg/ogu .gg/hackers .gg/hacking .gg/sim .gg/simswap .gg/simming .gg/stealing .gg/scamming
+// will make src code into bot leak soon :shush:
